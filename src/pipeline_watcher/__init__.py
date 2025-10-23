f820cceb2bf1cc829eb820b03e692f6f0fec750a
dataGladiator <kevin.raines@protonmail.com>
2025-10-23 16:08:25 -0500

    Package pipeline_file into init file.

--- src/pipeline_watcher/__init__.py
+++ src/pipeline_watcher/__init__.py
@@ -1,13 +1,14 @@
 from .core import (
     SCHEMA_VERSION,
-    StepStatus, Check, StepReport, FileReport, PipelineReport
+    StepStatus, Check, StepReport, FileReport, PipelineReport,
+    pipeline_file
 )
 from .io import atomic_write_json, dump_report
 from .clocks import now_utc
 
 __all__ = [
     "SCHEMA_VERSION",
-    "StepStatus", "Check", "StepReport", "FileReport", "PipelineReport",
+    "StepStatus", "Check", "StepReport", "FileReport", "PipelineReport", "pipeline_file",
     "atomic_write_json", "dump_report", "now_utc",
 ]
 
