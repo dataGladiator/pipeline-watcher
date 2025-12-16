364b0a07dfd769172477246ffd411bddce7c0613
dgladiator <kevin.raines@protonmail.com>
2025-12-16 12:12:29 -0500

    Clean up __all__ definition.

--- src/pipeline_watcher/__init__.py
+++ src/pipeline_watcher/__init__.py
@@ -8,7 +8,8 @@ from .clocks import now_utc
 
 __all__ = [
     "SCHEMA_VERSION",
-    "Status", "Check", "StepReport", "FileReport", "PipelineReport", "pipeline_file", "file_step", "pipeline_step",
+    "Status", "Check", "StepReport", "FileReport", "PipelineReport",
+    "pipeline_file", "file_step", "pipeline_step",
     "atomic_write_json", "dump_report", "now_utc",
 ]
 
